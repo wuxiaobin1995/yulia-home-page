ed5d0475bec20395f816e476dd3b88cb10772c8f
吴晓斌 <36044379+wuxiaobin1995@users.noreply.github.com>
2021-08-06 20:54:18 +0800

    .

--- src/views/video-submissions/index.vue
+++ src/views/video-submissions/index.vue
@@ -1,39 +1,150 @@
 <!--
  * @Author      : 吴晓斌
  * @Date        : 2021-07-30 17:03:36
- * @LastEditTime: 2021-08-06 00:36:53
+ * @LastEditTime: 2021-08-06 17:14:56
  * @Description : 视频投稿
 -->
 <template>
   <div class="container">
-    <div :style="bg" class="video-container">
+    <!-- 视频投稿 -->
+    <div :style="bgImg" class="video-container">
       <div class="video-wrapper">
+        <!-- 卡片容器 -->
         <a-tabs
           default-active-key="1"
-          size="large"
-          tab-position="right"
+          tab-position="top"
           @change="handleChangeTab"
         >
-          <a-tab-pane key="1">
-            <span slot="tab">
-              <a-icon type="apple" />
-              Tab 1
+          <!-- 原创曲 -->
+          <a-tab-pane key="1" class="tab-pane-wrapper">
+            <span slot="tab" :style="headStyle">
+              <a-icon type="gitlab" />
+              原创曲
             </span>
-            Content of Tab 1
+            <a-timeline :reverse="true">
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <a
+                    href="https://www.bilibili.com/video/BV1Pi4y1j7sR"
+                    target="_blank"
+                  >
+                    <div class="content-wrapper">
+                      <img
+                        :src="
+                          require('@/assets/image/视频封面/不可触碰的行星.jpg')
+                        "
+                        class="video-img"
+                        object-fit="cover"
+                      />
+                      <div class="text-wrapper">
+                        <div class="title">
+                          【原创曲】世事艰难
+                          我拥抱你【人声本家】尤利娅的一万粉纪念~
+                        </div>
+                        <div class="desc">
+                          本家投稿：BV1if4y1R7tr 人声本家：尤利娅Yulia
+                          词/曲：猿马行歌 混音：漆柚 曲绘：Ash 视频：果酱
+                          这是尤利娅的第一首原创曲，很感谢猿马老师可以写出这么温暖的歌，我第一次听的时候泪目了，于是就答应了接受这个挑战。
+                          在这期间我也很荣幸地达到了一万粉，还拥有了许多爱着我的水手们，我真的感到很幸运很幸运。
+                          自从出道之后，每次我难过的时候大家都在，就算什么都不说，我也知道你们会站在我的身前替我挡风遮雨~
+                          这次也换我来治愈大家吧！
+                          请记住，不论你是谁，你在哪里，难过或开心，我都在这里，
+                          紧紧地抱住你。
+                          “不要让那泪珠换来同样哭泣的云，任阴霾吞没希望的痕迹。”
+                          所以，请驻足于此听我唱一曲吧，我会把天晴的祝福给你~
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </a-card>
+              </a-timeline-item>
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <a
+                    href="https://www.bilibili.com/video/BV1KB4y1K7Nj"
+                    target="_blank"
+                  >
+                    <div class="content-wrapper">
+                      <img
+                        :src="
+                          require('@/assets/image/视频封面/不可触碰的行星.jpg')
+                        "
+                        class="video-img"
+                      />
+                      <div class="text-wrapper">
+                        <div class="title">
+                          【原创曲】不可触碰的行星【人声本家】Feat.可嗒
+                        </div>
+                        <div class="desc">
+                          大家早上好下午好晚上好！！
+                          今天带来的曲子《不可触碰的行星》是我久违地担当了词作的作品～
+                          也是@可嗒_Keda 专辑《Drifting to the
+                          Future》的打头作——
+                          我记得第一次听到这首歌是在半夜，可子突然给我发过来说“啊呀你听听我新歌！”
+                          我点开之后完全被旋律吸引住了，就问他我可不可以整个中文版，他说好呀好呀，写完我俩录！
+                          然后我用了三四个小时就写出了第一版词……
+                          可能真的是和这首歌很投缘吧，之后也没做什么改动直接就拿去录了demo。
+                          很幸运可以遇到这么会写的可达鸭，也很幸运自己可以遇见这么好的曲子唱！
+                          总之，希望听到这首歌的每一个你都能够喜欢～
+                          V家版本：BV1Ev411E7Ch staff：
+                          演唱：可嗒Keda&尤利娅Yulia 作曲/编曲：可嗒Keda
+                          作词：尤利娅Yulia
+                          吉他/母带：ZZM泽猫（泽猫giegie是神不用我多说了）
+                          后期：萨摩灰（一如既往的靠谱！！妈咪真的很有求必应呜呜呜）
+                          曲绘：浮途（超超超牛的途途！！拿到图的时候惊呆了，这就是超级大作吗？！）
+                          PV：K_Lacid（千千我的天使！！第一首原创曲就有这么高质量的pv真的很幸福！）
+                          封面：么羊（羊他真的很强！！约！！！真的好厉害！）
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </a-card>
+              </a-timeline-item>
+            </a-timeline>
           </a-tab-pane>
-          <a-tab-pane key="2">
-            <span slot="tab">
-              <a-icon type="apple" />
-              Tab 2
+
+          <!-- 翻唱曲 -->
+          <a-tab-pane key="2" class="tab-pane-wrapper">
+            <span slot="tab" :style="headStyle">
+              <a-icon type="audio" />
+              翻唱曲
             </span>
-            Content of Tab 2
+            <a-timeline>
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <p>
+                    Card 2
+                  </p>
+                </a-card>
+              </a-timeline-item>
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <p>Card 22</p>
+                </a-card>
+              </a-timeline-item>
+            </a-timeline>
           </a-tab-pane>
-          <a-tab-pane key="3">
-            <span slot="tab">
-              <a-icon type="apple" />
-              Tab 3
+
+          <!-- 整活 -->
+          <a-tab-pane key="3" class="tab-pane-wrapper">
+            <span slot="tab" :style="headStyle">
+              <a-icon type="reddit" />
+              整活
             </span>
-            Content of Tab 3
+            <a-timeline>
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <p>
+                    Card 3
+                  </p>
+                </a-card>
+              </a-timeline-item>
+              <a-timeline-item>
+                <a-card class="card-wrapper">
+                  <p>Card 33</p>
+                </a-card>
+              </a-timeline-item>
+            </a-timeline>
           </a-tab-pane>
         </a-tabs>
       </div>
@@ -47,9 +158,14 @@ export default {
 
   data() {
     return {
-      bg: {
+      // 根据标签切换，更改背景图
+      bgImg: {
         backgroundImage:
           'url(' + require('@/assets/image/背景图/通用背景图-3.jpg') + ')'
+      },
+      // 改变标签字体
+      headStyle: {
+        'font-size': '26px'
       }
     }
   },
@@ -57,17 +173,17 @@ export default {
   methods: {
     handleChangeTab(activeKey) {
       if (activeKey === '1') {
-        this.bg = {
+        this.bgImg = {
           backgroundImage:
             'url(' + require('@/assets/image/背景图/通用背景图-3.jpg') + ')'
         }
       } else if (activeKey === '2') {
-        this.bg = {
+        this.bgImg = {
           backgroundImage:
             'url(' + require('@/assets/image/背景图/通用背景图-4.jpg') + ')'
         }
       } else if (activeKey === '3') {
-        this.bg = {
+        this.bgImg = {
           backgroundImage:
             'url(' + require('@/assets/image/背景图/通用背景图-5.jpg') + ')'
         }
@@ -85,17 +201,50 @@ export default {
   /* 视频投稿 */
   .video-container {
     width: 100%;
+    min-height: 100vh;
     padding: 40px 0;
-    height: 2000px;
-    background: center center no-repeat fixed;
+    background: 50% 50% no-repeat fixed;
     background-size: cover;
     display: flex;
     justify-content: center;
 
     .video-wrapper {
       width: 80%;
-      background-color: rgba(255, 255, 255, 0.4);
-      border-radius: 20px;
+      background-color: rgba(255, 255, 255, 0.12);
+      border-radius: 16px;
+
+      .tab-pane-wrapper {
+        padding: 40px 120px;
+
+        .card-wrapper {
+          border-radius: 10px;
+          &:hover {
+            box-shadow: 0 0 4px 4px rgb(255, 115, 0); // 添加下阴影
+          }
+          .content-wrapper {
+            display: flex;
+            align-items: center;
+            .video-img {
+              width: 190px;
+              height: 119px;
+            }
+            .text-wrapper {
+              flex: 1;
+              color: #000;
+              padding-left: 20px;
+              .title {
+                font-size: 22px;
+                font-weight: 700;
+              }
+              .desc {
+                padding-top: 8px;
+                font-weight: lighter;
+                text-indent: 2em; // 首行缩进2字符
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
